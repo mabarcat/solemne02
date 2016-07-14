2197eff1290036ac8e08eadd1950d7b1c9e33759
mabarca <you@example.com>
2016-07-14 00:44:02 -0400

    logre los link en menus

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
-    url(r'^category/(?P<category_id>\d+)/$', views.category),
-    url(r'', views.index),
+    url(r'^category/(?P<category_id>\d+)/$', views.category, name='id'),
+    url(r'^$', views.index, name='index'),
  ]
